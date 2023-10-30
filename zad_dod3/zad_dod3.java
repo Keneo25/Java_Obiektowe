4d5e57c114b1515ff40d1ebb5151155b906fe0d4
Keneo25 <108327914+Keneo25@users.noreply.github.com>
2023-10-30 11:20:51 +0100

    Add files via upload

---
+++ zad_dod3/zad_dod3.java
@@ -0,0 +1,27 @@
+public class zad_dod {
+    public static void main(String[] args) {
+        Car1  car1 = new Car1();
+        Car1  car2 = new Car1();
+        car1.setHordePower(2);
+        String zmenna = car2.upgradeEngine(4);
+        System.out.println(zmenna);
+
+    }
+}
+    class Car1{
+        private int horsePower;
+
+
+        public void setHordePower(int value){
+            horsePower = value;
+
+        }
+
+        public String upgradeEngine(int value1){
+            return "String";
+
+        }
+
+    }
+
+
